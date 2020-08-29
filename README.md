a7ac311fa0684108fe01c86dd44a7cb41a2459ae
zdliuccit <335758663@qq.com>
2020-08-29 16:25:34 +0800

    feat: 初始化

---
+++ README.md
@@ -0,0 +1,7 @@
+# Koa服务
+
+## Package 说明
+* koa 主程
+* koa-router 路由
+* nodemon # nodemon是一种工具，可在检测到目录中的文件更改时通过自动重新启动节点应用程序来帮助开发基于node.js的应用程序
+    
--- packages/ml-cli/ml.js
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env node
-//--inspect-brk
-
-/**
- * /usr/bin/env node  可以让系统动态的去查找node，已解决不同机器不同用户设置不一致问题。
- * nodejs --inspect-brk结合Chrome开发者工具的调试
- */
-require('./lib/index.js');
-
